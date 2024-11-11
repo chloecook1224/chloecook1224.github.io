1703e60e68c7e3bb54d8379d8a19e5a9d14f4236
chloecook1224 <chco7840@colorado.edu>
2024-11-11 15:52:47 -0700

    last push

--- js/wa11.js
+++ js/wa11.js
@@ -1,4 +1,3 @@
-// Array of image filenames (correct path relative to the current folder)
 const imageFilenames = [
     '/img/cat_1.jpg',
     '/img/cat_2.jpg',
@@ -7,7 +6,6 @@ const imageFilenames = [
     '/img/cat_5.jpg'
   ];
   
-  // Object containing alt text for each image
   const altTexts = {
     '/img/cat_1.jpg': 'Closeup of a blue human eye',
     '/img/cat_2.jpg': 'Mountain landscape at sunset',
@@ -19,15 +17,13 @@ const imageFilenames = [
   // Reference to the thumb-bar div
   const thumbBar = document.querySelector('.thumb-bar');
   
-  // Loop through the array of image filenames to insert thumbnail images
   imageFilenames.forEach(filename => {
     const newImage = document.createElement('img');
-    newImage.src = filename;  // Correct image path
+    newImage.src = filename;  
     newImage.alt = altTexts[filename];
     thumbBar.appendChild(newImage);
   });
   
-  // Add a click event listener to each thumbnail
   thumbBar.querySelectorAll('img').forEach(img => {
     img.addEventListener('click', () => {
       const displayedImg = document.querySelector('.displayed-img');
@@ -36,7 +32,7 @@ const imageFilenames = [
     });
   });
   
-  // Reference to the darken/lighten button and overlay div
+
   const btn = document.querySelector('button.dark');
   const overlay = document.querySelector('.overlay');
   
@@ -47,13 +43,13 @@ const imageFilenames = [
       btn.classList.remove('dark');
       btn.classList.add('light');
       btn.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)'; // Apply darken overlay
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)'; 
     } else {
       // Switch to darken mode
       btn.classList.remove('light');
       btn.classList.add('dark');
       btn.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'; // Remove darken overlay
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'; 
     }
   });
   
